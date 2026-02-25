58c796a07b89bd42167f61ea3e906152c84a9e8f
Medha Karthik <143346227+mkartuncc@users.noreply.github.com>
2026-02-24 20:16:03 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -28,17 +28,17 @@ fav_genre.show(10) #show top 10
 avg_listen_time = logs_df.groupBy("user_id") \
 	.agg(avg("duration_sec").alias("avg_duration_sec"))
 avg_listen_time = avg_listen_time.orderBy(desc("avg_duration_sec"))
-avg_listen_time.show(10) 
+avg_listen_time.show(10) #show top 10
 
 # Task 3: Genre Loyalty Scores (total listen time per genre per user, ranked)
 genre_loyalty = logs_with_genre.groupBy("user_id", "genre") \
 	.agg(sum("duration_sec").alias("loyalty_score"))
 top_loyal_users = genre_loyalty.orderBy(desc("loyalty_score")).limit(10)
-top_loyal_users.show()
+top_loyal_users.show() #show top 10
 
 # Task 4: Identify users who listen between 12 AM and 5 AM
 from pyspark.sql.types import TimestampType
 logs_df = logs_df.withColumn("timestamp", to_timestamp("timestamp", "yyyy-MM-dd HH:mm:ss"))
 night_logs = logs_df.filter((hour(col("timestamp")) >= 0) & (hour(col("timestamp")) < 5))
 night_users = night_logs.select("user_id").distinct()
-night_users.show()
+night_users.show(night_users.count(),truncate=False) # Show all night users
